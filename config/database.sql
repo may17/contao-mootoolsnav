3f9e3db7fc1d215c25cc867fcd3a786b9e8cbc09
Andreas Schempp <andreas@schempp.ch>
2012-07-19 22:59:09 +0200

    Database field length was too short for the mouseover event

--- config/database.sql
+++ config/database.sql
@@ -13,6 +13,6 @@
 
 CREATE TABLE `tl_module` (
   `mootoolsnavLevel` smallint(5) unsigned NOT NULL default '0',
-  `mootoolsnavTrigger` varchar(8) NOT NULL default '',
+  `mootoolsnavTrigger` varchar(16) NOT NULL default '',
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
